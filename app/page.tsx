19654d35656bd37801483f62e33fb30f5e601420
nurhadyr <nr00910288@techmahindra.com>
2025-08-10 02:14:19 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,8 @@ export default function GestureApp() {
 
     landmarkerRef.current = await HandLandmarker.createFromOptions(vision, {
       baseOptions: {
-        modelAssetPath: '/models/hand_landmarker.task', // HARUS ada di public/models
+        modelAssetPath:
+          'https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task',
       },
       numHands: 1,
       runningMode: 'VIDEO',
